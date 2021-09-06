b9379dccb5f65275deb3fa9278d240b923c59691
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-09-06 17:41:56 +0530

    view-creation

--- firstrest/views.py
+++ firstrest/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from firstrest.models import Product
+from django.http import JsonResponse
 
 # Create your views here.
+def getAll(Request):
+    result=Product.objects.all()
+    return JsonResponse({'products':list(result.values())}) 
