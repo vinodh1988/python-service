37dc0891c9c994cd800aea99ac3e5fb9a868f313
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-09-07 16:53:31 +0530

    parameterized-get

--- firstrest/apis/views.py
+++ firstrest/apis/views.py
@@ -3,12 +3,22 @@ from firstrest.models import Product
 from rest_framework.decorators import api_view,APIView
 from rest_framework.response import Response
 from firstrest.apis.serializer import ProductSerializer
+from rest_framework import status
 
 class  ProductsAPI(APIView):
-    def get(self,request):
-        productlist=Product.objects.all()
-        result=ProductSerializer(productlist,many=True)
-        return Response(result.data)
+
+    def get(self,request,pk=None):
+        if(pk==None):
+            productlist=Product.objects.all()
+            result=ProductSerializer(productlist,many=True)
+            return Response(result.data)
+        else:
+            try:
+                item=Product.objects.get(pk=pk)
+                return Response(ProductSerializer(item).data)
+            except Product.DoesNotExist:
+                return Response({'error':'No Content'}, status=status.HTTP_204_NO_CONTENT)
+
     
     def post(self,request):
         item=ProductSerializer(data=request.data)
--- firstrest/urls.py
+++ firstrest/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from firstrest.apis.views import ProductsAPI
 
 urlpatterns = [
-    path('',ProductsAPI.as_view(),name="product-list")
+    path('',ProductsAPI.as_view(),name="product-list"),
+    path('<int:pk>',ProductsAPI.as_view(), name="product-by-id")
 ]
