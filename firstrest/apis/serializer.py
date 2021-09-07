c6d44a063dea4ecb779a45f21f52038f75d2cb20
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-09-07 16:25:46 +0530

    model-serialization

--- firstrest/apis/serializer.py
+++ firstrest/apis/serializer.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from firstrest.models import Product
-class ProductSerializer(serializers.Serializer):
-    productid=serializers.IntegerField()
-    name=serializers.CharField()
-    price=serializers.FloatField()
-    type=serializers.CharField()
+# class ProductSerializer(serializers.Serializer):
+#     productid=serializers.IntegerField()
+#     name=serializers.CharField()
+#     price=serializers.FloatField()
+#     type=serializers.CharField()
 
-    def create(self, data):
-        return Product.objects.create(**data)
-    
+#     def create(self, data):
+#         return Product.objects.create(**data)
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Product
+        fields = "__all__" 
+        #exclude=['productid']   
     
