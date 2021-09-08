2ccc66a997d1e0269a1453670d0b61f4e60b7b40
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-09-08 17:32:39 +0530

    filter

--- firstrest/apis/genericviews.py
+++ firstrest/apis/genericviews.py
@@ -3,5 +3,13 @@ from firstrest.apis.serializer import ProductSerializer
 from firstrest.models import Product
 
 class ProductGAPI(generics.ListAPIView):
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self): 
+        searchtype = self.kwargs['type']
+        #searchtype=self.request.query_params.get('type')
+        if searchtype==None:
+            return Product.objects.all()
+        print(searchtype,'#########')
+        return Product.objects.filter(type=searchtype)
--- firstrest/urls.py
+++ firstrest/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 from firstrest.apis.views import ProductsAPI,SuppliersAPI
-
+from firstrest.apis.genericviews import ProductGAPI
 urlpatterns = [
     path('products/',ProductsAPI.as_view(),name="product-list"),
     path('products/<int:pk>',ProductsAPI.as_view(), name="product-by-id"),
     path('suppliers/',SuppliersAPI.as_view(),name="supplier-get"),
-    path('market-products/',ProductsAPI.as_view(),name="product-generic")
+    path('market-products/',ProductGAPI.as_view(),name="product-generic"),
+    path('market-products/<str:type>/',ProductGAPI.as_view(), name="gproduct-by-type")
 ]
