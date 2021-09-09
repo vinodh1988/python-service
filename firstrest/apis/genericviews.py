1016f4a26fe46d58442a35788f6e1513a34840fd
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-09-09 14:43:36 +0530

    search-filter-commit

--- firstrest/apis/genericviews.py
+++ firstrest/apis/genericviews.py
@@ -1,12 +1,16 @@
 from rest_framework import generics
 from firstrest.apis.serializer import ProductSerializer
 from firstrest.models import Product
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 class ProductGAPI(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    #filter_backends = [DjangoFilterBackend]
+    filter_backends=[DjangoFilterBackend,filters.SearchFilter]
     filterset_fields = ['name', 'type']
-
+    search_fields=['name','type']  
     # def get_queryset(self): 
     #     #searchtype = self.kwargs['type']
     #     #searchtype=self.request.query_params.get('type')
--- projectweb/settings.py
+++ projectweb/settings.py
@@ -133,5 +133,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    #'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
