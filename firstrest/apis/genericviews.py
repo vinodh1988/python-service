61d728c6de340609eae7d7ee70f3ed37bcaf574f
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-09-09 14:27:22 +0530

    generic-filter-commit

--- firstrest/apis/genericviews.py
+++ firstrest/apis/genericviews.py
@@ -3,13 +3,14 @@ from firstrest.apis.serializer import ProductSerializer
 from firstrest.models import Product
 
 class ProductGAPI(generics.ListAPIView):
-    #queryset = Product.objects.all()
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filterset_fields = ['name', 'type']
 
-    def get_queryset(self): 
-        searchtype = self.kwargs['type']
-        #searchtype=self.request.query_params.get('type')
-        if searchtype==None:
-            return Product.objects.all()
-        print(searchtype,'#########')
-        return Product.objects.filter(type=searchtype)
+    # def get_queryset(self): 
+    #     #searchtype = self.kwargs['type']
+    #     #searchtype=self.request.query_params.get('type')
+    #     if searchtype==None:
+    #         return Product.objects.all()
+    #     print(searchtype,'#########')
+    #     return Product.objects.filter(type=searchtype)
--- projectweb/settings.py
+++ projectweb/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firstrest',
-    'rest_framework'
+    'rest_framework',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
